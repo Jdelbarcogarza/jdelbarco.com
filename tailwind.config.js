498f0d331d3fe399956818198540c66294ceeccb
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-21 01:03:03 -0500

    added xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      
+      screens: {
+        // custom tailwind utility para teléfonos más pequeños
+        'xs': '375px'
+      },
+
       fontFamily: {
         'Caveat': ['Caveat','cursive'],
       }
