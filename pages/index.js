8dda9b2322df0357cc0f35674184190005965f0f
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-23 13:23:27 -0500

    fixed text align

--- pages/index.js
+++ pages/index.js
@@ -20,19 +20,18 @@ export default function Home() {
 
 
         {/** Height de h-5/6 */}
-        <div className='h-5/6 w-full flex flex-col justify-around items-center 
-        md:flex-row-reverse
+        <div className='h-5/6 w-full flex flex-col justify-around items-center px-2
+        md:flex-row-reverse md:px-6
          border-2 border-lime-700'>
           
-          <div className=' 
-          w-36
+          <div className=' w-56 mt-4
           md:w-64'>
-            <Image src={HeroImage} alt='/' objectFit='cover' layout='intrinsic' />
+            <Image src={HeroImage} alt='/' objectFit='cover' layout='responsive' />
           </div>
 
-          <div className='flex flex-col content-center'>
-            <h1 className='text-2xl md:text-5xl'><strong>I love</strong> building exciting <strong>software</strong></h1>
-            <button className='w-fit place-self-center text-lg md:self-start md:text-2xl bg-teal-800 p-2 my-8 rounded-md'>Contact Me</button>
+          <div className='flex flex-col content-center mb-16 md:mb-0 '>
+            <h1 className=' text-center text-2xl sm:text-3xl md:text-5xl md:text-left'><strong>I love</strong> building exciting <strong>software</strong></h1>
+            <button className='w-fit place-self-center text-lg md:self-start sm:text-2xl md:text-2xl bg-teal-800 p-2 my-8 rounded-md'>Contact Me</button>
           </div>
         </div>
 
