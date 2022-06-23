e68486520b2488f4c6afaff2bfbb61e27ff4993e
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-23 12:31:37 -0500

    added navar to hero section code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Navbar } from '../components/Navbar'
-import HeroImage from '../public/images/HeroImage.jpg'
+import HeroImage from '../public/images/Coding_PNG.png'
 import { SectionTitle } from '../components/Sectiontitle'
 
 export default function Home() {
@@ -13,19 +13,33 @@ export default function Home() {
 
       <section className='h-full w-full text-white'>
 
+
         {/** Height de h-1/6 */}
         <Navbar />
 
+
+
         {/** Height de h-5/6 */}
-        <div className='h-5/6 w-full flex flex-col justify-center items-center pl-8 border-2 border-lime-700'>
-          <h1 className='text-5xl'><strong>I love</strong> building exciting <strong>software</strong></h1>
-          <button className='text-2xl bg-teal-800 p-2 mt-8 rounded-md'>Contact Me</button>
+        <div className='h-5/6 w-full flex flex-col justify-around items-center 
+        md:flex-row-reverse
+         border-2 border-lime-700'>
+          
+          <div className=' 
+          w-36
+          md:w-64'>
+            <Image src={HeroImage} alt='/' objectFit='cover' layout='intrinsic' />
+          </div>
+
+          <div>
+            <h1 className='text-2xl md:text-5xl'><strong>I love</strong> building exciting <strong>software</strong></h1>
+            <button className='text-lg md:text-2xl bg-teal-800 p-2 mt-8 rounded-md'>Contact Me</button>
+          </div>
         </div>
 
       </section>
 
 
-    {/** Section: My Work */}
+      {/** Section: My Work */}
       <section className='h-full w-full bg-black px-16 pt-4'>
 
         <SectionTitle title={'My Work'} />
