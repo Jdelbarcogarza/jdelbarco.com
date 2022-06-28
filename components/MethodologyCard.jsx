fe324179d67e3a08437ec18c03f1c2fe730fc1d4
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-28 14:59:03 -0500

    created component my skills

---
+++ components/MethodologyCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MethodologyCard = ({image, title, description}) => {
+  return (
+    <div className='flex flex-col text-white'>
+    MethodologyCard
+    
+    <p>{title}</p>
+    <p>{description}</p>
+    
+    </div>
+  )
+}
+
