442b34566da2838ad142b12db17f864038bbc0ae
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-28 14:59:35 -0500

    added work section

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,20 @@ import Image from 'next/image'
 import { Navbar } from '../components/Navbar'
 import HeroImage from '../public/images/Coding_PNG.png'
 import { SectionTitle } from '../components/Sectiontitle'
+import { MethodologyCard } from '../components/MethodologyCard'
+
+// import of images for work section (methodology part)
+import Manage from '../public/images/workSectionSVG/manage.svg'
 
 export default function Home() {
 
   const webPageSections = ["Home", "My Work", "Contact Me"]
+
+  const workMethodology = [
+    {title: "Manage software projects", image: Manage,
+     description: "Esta es la descripción de mi work section\
+      que se ve bien padre y debe ser algo corta"}
+  ]
 
   return (
     <main className='h-screen w-full text-white bg-black'>
@@ -26,7 +36,7 @@ export default function Home() {
 
           <div className=' w-56 mt-4
           md:w-72'>
-            <Image src={HeroImage} alt='/' objectFit='cover' layout='responsive' />
+            <Image src={HeroImage} alt='/' objectFit='cover' layout='responsive' priority/>
           </div>
 
           <div className='flex flex-col content-center mb-16 md:mb-0 w-fit'>
@@ -38,10 +48,23 @@ export default function Home() {
       </section>
 
 
-      {/** Section: My Work */}
+      {/** Section: My Work 
+           HEX COLOR USED FOR UNDRAW SVGs: #07A291      
+      */}
       <section className='h-full w-full bg-black px-16 pt-4'>
 
         <SectionTitle title={'My Work'} />
+
+        {workMethodology.map((obj ,id) => {
+          return (
+          <MethodologyCard 
+          key={id}
+          title={obj.title}
+          section={obj.section}
+          description={obj.description}
+            />
+          )
+        })}
 
       </section>
 
