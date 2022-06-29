eeb663b791451157ed4fc3916a9d7ee160511fb3
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-06-29 01:07:45 -0500

    methodology cells complete

--- components/MethodologyCard.jsx
+++ components/MethodologyCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
-export const MethodologyCard = ({image, title, description}) => {
+export const MethodologyCard = ({ image, title, description }) => {
   return (
-    <div className='flex flex-col text-white'>
-    MethodologyCard
-    
-    <p>{title}</p>
-    <p>{description}</p>
-    
+
+    <div className='flex flex-col text-white h-full items-center'>
+
+      <p className='text-xl my-6 font-semibold' >{title}</p>
+
+        {/** Photo and description */}
+      <div className='h-full flex flex-col justify-center ' >
+        <div className='w-1/2 md:h-72 justify-center self-center max-w-xs '>
+          <Image src={image} alt='/' objectFit='cover' layout='responsive' priority />
+        </div>
+        <p className='whitespace-normal max-w-prose slef-center xs:mt-8'>{description}</p>
+      </div>
     </div>
   )
 }
