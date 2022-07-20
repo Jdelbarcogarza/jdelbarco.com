468ca43698b9a223dda217c67ab26e4e8b99befd
Jdelbarco <jdelbarcogarza@gmail.com>
2022-07-20 13:38:08 -0500

    changed section title <hr> bottom margin

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const SectionTitle = ({title}) => {
   return (
     <div className='h-fit w-fit'>
-          <hr className='bg-slate-400 h-0.5 w-3/4' />
+          <hr className='bg-slate-400 h-0.5 w-3/4 mb-1' />
           <h4 className='text-3xl'>{title}</h4>
         </div>
   )
