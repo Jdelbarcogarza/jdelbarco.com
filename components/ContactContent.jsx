67e96c5f197cc018457a0df82cfab4bdcb5498cd
Jdelbarco <jdelbarcogarza@gmail.com>
2022-07-25 13:10:29 -0500

    finished quote section

--- components/ContactContent.jsx
+++ components/ContactContent.jsx
@@ -6,20 +6,24 @@ import { SocialIcons } from '../components/SocialIcons'
 
 export const ContactContent = ({ title, quote, author, children }) => {
   return (
+    <>
+      
+      <div className='pb-8 p-4 flex flex-col gap-16'>
 
-    <div className='p-8 flex flex-col justify-between'>
-      <h1 className='text-2xl'>{title}</h1>
-      <article className=''>
-        <p className=''>{quote}</p>
-        <p>{author}</p>
-      </article>
-      <div className='prose dark:prose-invert prose-slate'>
-        <ReactMarkdown>{children}</ReactMarkdown>
+        <article className=''>
+          <p className='italic text-2xl font-medium'>{quote}</p>
+          <p className='text-xl '>&#8212; {author}</p>
+        </article>
+        {/*
+        <div className='prose dark:prose-invert prose-slate'>
+          <ReactMarkdown>{children}</ReactMarkdown>
+        </div>
+        */}
+
+        <div className=''>
+          <SocialIcons />
+        </div>
       </div>
-
-
-        <SocialIcons />
-    </div>
-
+    </>
   )
 }
