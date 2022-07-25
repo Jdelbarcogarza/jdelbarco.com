5b4badb15f3fc7498aa9f7065e59fb642c6f5603
Jdelbarco <jdelbarcogarza@gmail.com>
2022-07-25 12:06:32 -0500

    added placeholder content for contact section

--- components/ContactContent.jsx
+++ components/ContactContent.jsx
@@ -1,12 +1,25 @@
 
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import { SocialIcons } from '../components/SocialIcons'
 
 
-export const ContactContent = ({children}) => {
+export const ContactContent = ({ title, quote, author, children }) => {
   return (
-    <div>
-    <ReactMarkdown>{children}</ReactMarkdown>
+
+    <div className='p-8 flex flex-col justify-between'>
+      <h1 className='text-2xl'>{title}</h1>
+      <article className=''>
+        <p className=''>{quote}</p>
+        <p>{author}</p>
+      </article>
+      <div className='prose dark:prose-invert prose-slate'>
+        <ReactMarkdown>{children}</ReactMarkdown>
+      </div>
+
+
+        <SocialIcons />
     </div>
+
   )
 }
