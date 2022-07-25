ddeff5bea4d65c2e94939ce13853117b74a5af88
Jdelbarco <jdelbarcogarza@gmail.com>
2022-07-25 12:05:58 -0500

    added webpack svgr to config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,16 @@ const nextConfig = {
     deviceSizes: [320, 420, 768, 1024, 1200],
     loader:  'default',
     domains: ['localhost','res.cloudinary.com'],
-  }
+  },
+  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
