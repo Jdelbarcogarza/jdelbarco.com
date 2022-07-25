ad25b802d1c0b241a6f12ceafb25df43c4982a18
Jdelbarco <jdelbarcogarza@gmail.com>
2022-07-25 12:06:46 -0500

    created social icons component

---
+++ components/SocialIcons.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import SpotifyIcon from '../public/spotify.svg'
+import GithubIcon from '../public/github.svg'
+import LinkedinIcon from '../public/linkedin.svg'
+
+
+export const SocialIcons = () => {
+  return (
+    <div>
+      <SpotifyIcon />
+      <GithubIcon />
+      <LinkedinIcon />
+    </div>
+  )
+}
